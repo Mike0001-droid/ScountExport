a38d83a747af2f4fc17a5ce40b67e98c19f7d7d7
Mike0001-droid <majorov-2004@list.ru>
2024-11-05 02:56:12 +0500

    Изменил путь до статики на проде

--- scount/scount/settings.py
+++ scount/scount/settings.py
@@ -121,7 +121,7 @@ if DEBUG:
     STATIC_DIR = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = [
         STATIC_DIR,
-        '/var/www/static/',
+        '/var/www/ScountExport/scount/static/',
     ]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
